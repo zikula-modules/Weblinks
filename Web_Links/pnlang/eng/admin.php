91703fa8bf6f6c5979eabaa890be7925ee4f6dae
Petzi-Juist <Petzi-Juist@77007f8e-315d-4fb3-849a-97b0e9da9dc0>
2008-11-12 21:21:10 +0000

    first commit for 2.0

---
+++ Web_Links/pnlang/eng/admin.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Zikula Application Framework
+ *
+ * Web_Links
+ *
+ * @version $Id$
+ * @copyright 2008 by Petzi-Juist
+ * @link http://www.petzi-juist.de
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ */
+define('_WL_ADDCATEGORY', 'Kategorie hinzufügen');
+define('_WL_ADDCATEGORYSUCCESSFULY', 'Kategorie erfolgreich hinzugefügt');
+define('_WL_ADDLINK', 'Link hinzufügen');
+define('_WL_ANONPOSTLINKS', 'Berechtigung zur Meldung für anonyme Benutzer?');
+define('_WL_ANONWAITDAYS', 'Tagesanzahl bis anonyme Benutzer einen Link bewerten können');
+define('_WL_ANONWEIGHT', 'Wieviele Stimmen unregistrierter Benutzer pro 1 Stimme registrierter Benutzer?');
+define('_WL_BLOCKUNREGMODIFY', 'unregistrierten Benutzern das Vorschlagen von Änderungen verbieten?');
+define('_WL_BROKENLINKSREP', 'als defekt gemeldete Links');
+define('_WL_CATEGORY', 'Kategorie');
+define('_WL_CATVIEW', 'Kategorien verwalten');
+define('_WL_CONF', 'Konfirguration');
+define('_WL_CONFIGUPDATED', 'Konfiguration aktualisiert');
+define('_WL_DAYS', 'Tage');
+define('_WL_DBSTATUS', 'Datenbankbestand');
+define('_WL_DELCATEGORY', 'Kategorie löschen');
+define('_WL_DELCATSUCCESSFULY', 'Kategorie erfolgreich gelöscht');
+define('_WL_DELCATWARNING', 'WARNUNG: Wirklich die Kategorie und alle darin befindlichen Links löschen?');
+define('_WL_DELLINK', 'Link löschen');
+define('_WL_DELLINKSUCCESSFULY', 'Link aus der Datenbank entfernt');
+define('_WL_DESCRIPTION', 'Beschreibung');
+define('_WL_DESCRIPTION255', 'Beschreibung: (max. 255 Zeichen)');
+define('_WL_DETAILVOTEDECIMAL', 'Dezimalstellen bei detailliertem Abstimmungsergebnis');
+define('_WL_EDITORIALADDED', 'Editorial in die Datenbank aufgenommen');
+define('_WL_EDITORIALDELETED', 'Editorial aus der Datenbank entfernt');
+define('_WL_EDITORIALMODIFIED', 'Editorial modifiziert');
+define('_WL_EMAIL', 'E-Mail');
+define('_WL_ERRORNODESCRIPTION', 'FEHLER: Beschreibung für den Link angeben!');
+define('_WL_ERRORNOTITLE', 'FEHLER: Titel für den Link angeben!');
+define('_WL_ERRORNOURL', 'FEHLER: URL für den Link angeben!');
+define('_WL_ERRORTHECATEGORY', 'FEHLER: Die Kategorie konnte nicht angelegt werden.');
+define('_WL_ERRORURLEXIST', 'FEHLER: Die URL existiert bereits in der Datenbank!');
+define('_WL_FEATUREBOX', 'Featured Link-Box auf der Hauptseite anzeigen?');
+define('_WL_GOBACK', 'Zurück');
+define('_WL_HELLO', 'Hallo');
+define('_WL_HITS', 'Hits');
+define('_WL_IGNORELINKSUCCESSFULY', 'Meldung ignoriert');
+define('_WL_INCAT', 'in der Kategorie');
+define('_WL_LINKID', 'Link-ID');
+define('_WL_LINKMODREQUEST', 'Link-Änderungswünsche');
+define('_WL_LINKSASBEST', 'Anzahl der besten Links');
+define('_WL_LINKSASNEW', 'Anzahl der neuen Links');
+define('_WL_LINKSINDB', 'Links in der Datenbank');
+define('_WL_LINKSINRES', 'Anzahl der Links in Suchergebnis');
+define('_WL_LINKSPAGE', 'Links pro Seite');
+define('_WL_LINKSWAITINGVAL', 'auf Freischaltung wartenden Links');
+define('_WL_LINKTITLE', 'Link Titel');
+define('_WL_LINKVIEW', 'Links verwalten');
+define('_WL_LINKVOTEMIN', 'notwendige Stimmenanzahl für die \'Top 10\'-Liste');
+define('_WL_MAINVOTEDECIMAL', 'Dezimalstellen des Stimmergebnisses');
+define('_WL_MODCATEGORY', 'Kategorie modifizieren');
+define('_WL_MODCONF', 'Konfiguration ändern');
+define('_WL_MODIFYCATSUCCESSFULY', 'Kategorie erfolgreich modifiziert');
+define('_WL_MODIFYLINKSUCCESSFULY', 'Link erfolgreich modifiziert');
+define('_WL_MODLINK', 'Link modifizieren');
+define('_WL_MOSTPOPLINKS', 'beliebteste Links: Anzahl oder Prozentsatz zur Anzeige');
+define('_WL_MOSTPOPLINKSPERCENTRIGGER', '1 für Prozentsatz (ansonsten Anzahl)');
+define('_WL_NAME', 'Name');
+define('_WL_NEWLINKADDED', 'neuer Link in die Datenbank aufgenommen');
+define('_WL_NEWLINKDELETED', 'neuer Link wurde gelöscht');
+define('_WL_NO', 'Nein');
+define('_WL_NONE', 'keine');
+define('_WL_OUTSIDEWAITDAYS', 'Tagesanzahl bis externe Benutzer Stimmen abgeben können');
+define('_WL_OUTSIDEWEIGHT', 'Wieviele externe Stimmen pro 1 Stimme registrierter Benutzer?');
+define('_WL_OVERVIEW', 'Übersicht');
+define('_WL_PAGETITLE', 'Seitentitel');
+define('_WL_PAGEURL', 'URL');
+define('_WL_SUBMITTER', 'eingereicht von');
+define('_WL_TEAM', 'Team');
+define('_WL_THANKS4YOURSUBMISSION', 'Vielen Dank für den Eintrag!');
+define('_WL_THEREARE', 'Es gibt');
+define('_WL_TOBEPOPULAR', 'Hits für Beliebtheit');
+define('_WL_TOPLINKS', 'Top Links: Anzahl oder Prozentsatz zur Anzeige');
+define('_WL_TOPLINKSPERCENTRIGGER', '1 für Prozentsatz (ansonsten Anzahl)');
+define('_WL_URL', 'URL');
+define('_WL_USEOUTSIDEVOTING', 'externe Stimmabgabe erlauben');
+define('_WL_VISIT', 'Besuche URL');
+define('_WL_WARNING', '!ACHTUNG! - Dies ist nur eine SVN-Version und noch nicht fertig.<br />Bugs bitte nicht im cozi sondern direkt an <a href="http://www.petzi-juist.de/module-formicula.phtml">mich</a> melden.');
+define('_WL_WEAPPROVED', 'Der folgende Eintrag wurde in die Weblinks aufgenommen: ');
+define('_WL_WEBLINKS', 'Weblinks');
+define('_WL_YES', 'Ja');
+define('_WL_YOUCANBROWSEUS', 'unsere Weblinks online: ');
+define('_WL_YOURLINKAT', 'Link bei');
+?>
