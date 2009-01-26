8fb85eda536d7ab5189fa7a774ea692fec7c2286
Petzi-Juist <Petzi-Juist@77007f8e-315d-4fb3-849a-97b0e9da9dc0>
2009-01-26 12:32:31 +0000

    little changes

--- Web_Links/pntemplates/plugins/function.ezcommentscounter.php
+++ Web_Links/pntemplates/plugins/function.ezcommentscounter.php
@@ -10,15 +10,6 @@
  * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
  */
 
-/**
- * Smarty function to obtain the EZComments counter
- *
- * @author       Andreas Krapohl
- * @since        15.07.04
- * @param        array       $params      All attributes passed to this function from the template
- * @param        object      &$smarty     Reference to the Smarty object
- * @return       string      the comments counter
- */
 function smarty_function_ezcommentscounter($params, &$smarty)
 {
     // get the parameters
@@ -26,34 +17,26 @@ function smarty_function_ezcommentscounter($params, &$smarty)
     unset($params);
 
     if (!isset($lid)) {
-        $smarty->trigger_error('ezcommentscounter: attribute sid required');
+        $smarty->trigger_error('ezcommentscounter: attribute lid required');
         return false;
     }
 
     // set default
     $ezcommentscounter = "";
 
-    // Security check
-    if (!pnSecAuthAction(0, 'EZComments::', "Web_Links:$lid:", ACCESS_READ)) {
-        return $ezcommentsounter;
+    if (pnModAvailable('EZComments') && pnModIsHooked('EZComments', 'Web_Links')) {
+        $items  = pnModAPIFunc('EZComments', 'user', 'getall',
+                               array('mod' => 'Web_Links',
+                                     'status' => 0,
+                                     'objectid' => $lid));
+
+        $ezcommentscounter = count($items);
     }
 
-    if (pnModAvailable('EZComments'))   {
-        $dbconn =& pnDBGetConn(true);
-        pnModDBInfoLoad("EZComments");
-        $pntable =& pnDBGetTables();
-        $EZCommentstable = $pntable['EZComments'];
-        $EZCommentscolumn = &$pntable['EZComments_column'];
-        $sql = "SELECT COUNT(1) FROM $EZCommentstable WHERE $EZCommentscolumn[modname] = 'Web_Links' AND $EZCommentscolumn[objectid] = '$lid'";
-        $result =& $dbconn->Execute($sql);
-        list($ezcommentscounter) = $result->fields;
+    if ($ezcommentscounter=="0") $comments = _WL_FEELFREE2ADD;
+    elseif ($ezcommentscounter=="1") $comments = "1 "._WL_COMMENT;
+    else $comments = "$ezcommentscounter "._WL_COMMENTS;
 
-        if ($ezcommentscounter=="0") $comments = _WL_FEELFREE2ADD;
-        elseif ($ezcommentscounter=="1") $comments = "1 "._WL_COMMENT;
-        else $comments = "$ezcommentscounter "._WL_COMMENTS;
-
-
-    }
     if (isset($assign)) {
         $smarty->assign($assign, $comments);
     } else {
