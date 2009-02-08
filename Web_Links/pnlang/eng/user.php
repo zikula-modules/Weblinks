67e8cbd5c76ebfdfe6280ab37028f991a8153900
Petzi-Juist <Petzi-Juist@77007f8e-315d-4fb3-849a-97b0e9da9dc0>
2009-02-08 16:59:25 +0000

    bugfix #25

--- Web_Links/pnlang/eng/user.php
+++ Web_Links/pnlang/eng/user.php
@@ -65,7 +65,7 @@ define('_WL_LINKSNOTUSER4','Why is registration required for access to certain f
 define('_WL_LINKSNOTUSER5','So that you are offered only the highest-quality content,');
 define('_WL_LINKSNOTUSER6','each item is individually reviewed and approved.');
 define('_WL_LINKSNOTUSER7','The site editor aims to offer useful information only.');
-define('_WL_LINKSNOTUSER8','<a href=\"index.php?module=Users&func=register\">Register for a new user account</a>');
+define('_WL_LINKSNOTUSER8','<a href="index.php?module=Users&func=register">Register for a new user account</a>');
 define('_WL_LINKTITLE','Link title');
 define('_WL_LOOKTOREQUEST', 'Thank you! Your request has been submitted for examination.');
 define('_WL_LOWRATING', 'Low rating');
