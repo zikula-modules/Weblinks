856b981397920f36756e61e7b177e56ec1e535a6
Craig Heydenburg <craigh@mac.com>
2012-11-14 20:10:13 -0500

    write upgrade routine

--- src/modules/Weblinks/lib/Weblinks/Installer.php
+++ src/modules/Weblinks/lib/Weblinks/Installer.php
@@ -60,8 +60,6 @@ class Weblinks_Installer extends Zikula_AbstractInstaller
      */
     public function Upgrade($oldversion)
     {
-        // Get database information
-        $dbtable = DBUtil::getTables();
 
         switch ($oldversion) {
             case '1.0':
@@ -94,47 +92,10 @@ class Weblinks_Installer extends Zikula_AbstractInstaller
                     ModUtil::delVar('Web_Links');
                 }
 
-                // rename hook
-                $dbtable = DBUtil::getTables();
-                $hookscolumn = $dbtable['hooks_column'];
-                $object = array('smodule' => 'Weblinks');
-                $where = "WHERE $hookscolumn[smodule] = 'Web_Links'";
-                DBUtil::updateObject($object, 'hooks', $where, 'id');
-
-                // rename hooks entries
-                if (ModUtil::available('EZComments')) {
-                    ModUtil::dbInfoLoad('EZComments');
-                    $dbtable = DBUtil::getTables();
-                    $ezccolumn = $dbtable['EZComments_column'];
-                    $where = "WHERE $ezccolumn[modname] = 'Web_Links'";
-                    $ezcarray = DBUtil::selectObjectArray('EZComments', $where, '', -1, -1, false);
-                    $ezccount = count($ezcarray);
-                    for ($cnt = 0; $cnt < $ezccount; $cnt++) {
-                        $ezcarray[$cnt]['modname'] = str_replace('Web_Links', 'Weblinks', $ezcarray[$cnt]['modname']);
-                    }
-                    DBUtil::updateObjectArray($ezcarray, 'EZComments', 'id');
-                }
-
-                if (ModUtil::available('Ratings')) {
-                    ModUtil::dbInfoLoad('Ratings');
-                    $dbtable = DBUtil::getTables();
-                    $ratcolumn = $dbtable['ratings_column'];
-                    $where = "WHERE $ratcolumn[module] = 'Web_Links'";
-                    $ratarray = DBUtil::selectObjectArray('ratings', $where, '', -1, -1, false);
-                    $ratcount = count($ratarray);
-                    for ($cnt = 0; $cnt < $ratcount; $cnt++) {
-                        $ratarray[$cnt]['module'] = str_replace('Web_Links', 'Weblinks', $ratarray[$cnt]['module']);
-                    }
-                    DBUtil::updateObjectArray($ratarray, 'ratings', 'rid');
-                }
+                // NOTE: old hooks rename is removed from this upgrade (here)
+                // because old hooks system no longer used in v3.0.0
 
             case '2.0.1':
-
-                if (version_compare(Zikula_Core::VERSION_NUM, '1.2.0', '<')) {
-                    SessionUtil::setVar('errormsg', $this->__('Error! This version of the Weblinks module requires Zikula 1.2.0 or later. Installation has been stopped because this requirement is not met.'));
-                    return false;
-                }
-
                 $this->setVar('doubleurl', 0);
                 $this->setVar('unregbroken', 0);
                 $this->setVar('thumber', 0);
@@ -145,17 +106,103 @@ class Weblinks_Installer extends Zikula_AbstractInstaller
                 $this->delVar('toplinkspercentrigger');
 
             case '2.1.0':
-                // not released
+                // version 2.1.0 was not released
+                $connection = $this->entityManager->getConnection();
+                $prefix = $this->serviceManager['prefix'];
+                $prefix = (empty($prefix)) ? '' : "_" . $prefix;
+                $sqls = array();
+                // correct the links_link table
+                $sqls[] = 'RENAME TABLE ' . $prefix . 'links_link' . " TO links_link";
+                $sqls[] ="ALTER TABLE `links_links` CHANGE `pn_lid` `lid` INT(11) NOT NULL AUTO_INCREMENT, 
+CHANGE `pn_cat_id` `cat_id` INT(11) NOT NULL DEFAULT '0', 
+CHANGE `pn_title` `title` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '', 
+CHANGE `pn_url` `url` VARCHAR(254) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '', 
+CHANGE `pn_description` `description` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL, 
+CHANGE `pn_date` `date` DATETIME NULL DEFAULT NULL, 
+CHANGE `pn_name` `name` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '', 
+CHANGE `pn_email` `email` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '', 
+CHANGE `pn_hits` `hits` INT(11) NOT NULL DEFAULT '0', 
+CHANGE `pn_submitter` `submitter` VARCHAR(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '', 
+CHANGE `pn_ratingsummary` `ratingsummary` DOUBLE(6,4) NOT NULL DEFAULT '0.0000', 
+CHANGE `pn_totalvotes` `totalvotes` INT(11) NOT NULL DEFAULT '0',
+CHANGE `pn_totalcomments` `totalcomments` INT(11) NOT NULL DEFAULT '0'";
+                // correct the links_categories table
+                $sqls[] = 'RENAME TABLE ' . $prefix . 'links_categories' . " TO links_categories";
+                $sqls[] = "ALTER TABLE  `links_categories` CHANGE  `pn_cat_id`  `cat_id` INT( 11 ) NOT NULL AUTO_INCREMENT ,
+CHANGE  `pn_parent_id`  `parent_id` INT( 11 ) NULL DEFAULT NULL ,
+CHANGE  `pn_title`  `title` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '',
+CHANGE  `pn_description`  `description` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL";
+                foreach ($sqls as $sql) {
+                    $stmt = $connection->prepare($sql);
+                    try {
+                        $stmt->execute();
+                    } catch (Exception $e) {
+                        LogUtil::registerError($e->getMessage());
+                    }
+                }
+                // update to current entities (adds some columns) & set status to active
+                DoctrineHelper::updateSchema($this->entityManager, array('Weblinks_Entity_Link', 'Weblinks_Entity_Category'));
+                $sql = "UPDATE links_links SET status=" . Weblinks_Entity_Link::ACTIVE;
+                $stmt = $connection->prepare($sql);
+                $stmt->execute();
+
+                // move links from 'links_newlink' to 'links_link' with status INACTIVE
+                $sql = "SELECT * FROM {$prefix}links_newlink";
+                $objects = $connection->fetchAll($sql);
+                foreach ($objects as $object) {
+                    $link = new Weblinks_Entity_Link();
+                    $link->setTitle($object['pn_title']);
+                    $link->setDescription($object['pn_description']);
+                    $link->setUrl($object['pn_url']);
+                    $link->setName($object['pn_name']);
+                    $link->setEmail($object['pn_email']);
+                    $link->setSubmitter($object['pn_submitter']);
+                    $link->setStatus(Weblinks_Entity_Link::INACTIVE);
+                    $link->setCategory($this->entityManager->find('Weblinks_Entity_Category', $object['pn_cat_id']));
+                    $this->entityManager->persist($link);
+                }
+                $this->entityManager->flush();
+                // remove 'links_newlink' table
+                $sql = "DROP TABLE `{$prefix}links_newlink`";
+                $stmt = $connection->prepare($sql);
+                $stmt->execute();
+                
+                // move links from 'links_modrequest' to 'links_links' into the '$modifiedContent' column as serialized array
+                // with status ACTIVE_MODIFIED
+                $sql = "SELECT * FROM {$prefix}links_modrequest";
+                $objects = $connection->fetchAll($sql);
+                foreach ($objects as $object) {
+                    $link = $this->entityManager->find('Weblinks_Entity_Link', $object['lid']);
+                    $link->setModifysubmitter($object['modifysubmitter']);
+                    if ($object['pn_brokenlink'] == 1) {
+                        $link->setStatus(Weblinks_Entity_Link::ACTIVE_BROKEN);
+                    } else {
+                        $link->setModifiedContent(array(
+                            'title' => 'pn_title',
+                            'url' => 'pn_url',
+                            'description' => 'pn_description',
+                            'cat_id' => 'pn_cat_id',
+                        ));
+                        $link->setStatus(Weblinks_Entity_Link::ACTIVE_MODIFIED);
+                    }
+                    $this->entityManager->persist($link);
+                }
+                $this->entityManager->flush();
+                // remove 'links_modrequest' table
+                $sql = "DROP TABLE `{$prefix}links_modrequest`";
+                $stmt = $connection->prepare($sql);
+                $stmt->execute();
+                // remove 'links_votedata' table
+                $sql = "DROP TABLE IF EXISTS `{$prefix}links_votedata`";
+                $stmt = $connection->prepare($sql);
+                $stmt->execute();
+                // remove 'links_editorials' table
+                $sql = "DROP TABLE IF EXISTS `{$prefix}links_editorials`";
+                $stmt = $connection->prepare($sql);
+                $stmt->execute();
+                
                 // register new hooks
                 HookUtil::registerSubscriberBundles($this->version->getHookSubscriberBundles());
-                // need to move links from 'links_newlink' to 'links_link' with status INACTIVE
-                // then remove 'links_newlink' table
-                // need to move links from 'links_modrequest' to 'links_links' into the '$modifiedContent' column as serialized object
-                // then remove 'links_modrequest' table
-                // remove 'links_votedata' table
-                // remove 'links_editorials' table
-                // change 'links_links' table to match new Entity
-            
             case '3.0.0':
                 // future code
         }
@@ -168,26 +215,11 @@ class Weblinks_Installer extends Zikula_AbstractInstaller
      */
     public function Uninstall()
     {
-        // Delete tables
-
-        if (!DBUtil::dropTable('links_categories')) {
-            return false;
-        }
-
-        if (!DBUtil::dropTable('links_links')) {
-            return false;
-        }
-
-        if (!DBUtil::dropTable('links_modrequest')) {
-            return false;
-        }
-
-        if (!DBUtil::dropTable('links_newlink')) {
-            return false;
-        }
-
-        // remove module vars
-        ModUtil::delVar('Weblinks');
+        //drop the tables
+        DoctrineHelper::dropSchema($this->entityManager, array(
+            'Weblinks_Entity_Link',
+            'Weblinks_Entity_Category'));
+        $this->delVars();
 
         // remove hooks
         HookUtil::unregisterSubscriberBundles($this->version->getHookSubscriberBundles());
